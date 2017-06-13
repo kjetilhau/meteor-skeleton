7f5aba4ca61bf4a4ebb5ee58dc88d62393083c18
Kjetil Haugland <kjetilda@gmail.com>
2017-06-13 08:36:40 +0200

    Updated to Meteor 1.5

--- imports/modules/helpers.js
+++ imports/modules/helpers.js
@@ -45,7 +45,7 @@ export const bytesToSize = (bytes) => {
     return ('')
   } else {
     let sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
-    if (bytes == 0) return 'n/a'
+    if (bytes === 0) return 'n/a'
     let i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
     return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i]
   }
