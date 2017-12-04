7299350bedb98969935063240aa944b5552f212a
mr <fducom@gmail.com>
2017-12-04 20:12:16 +0200

    Added i18n package and (add popper npm dependency for "dropdown-toggle")

--- imports/ui/shared/header/header.js
+++ imports/ui/shared/header/header.js
@@ -1,1 +1,17 @@
+import { Template } from "meteor/templating"
+
 import './header.html'
+
+Template.header.onCreated(function() {})
+
+Template.header.onRendered(function() {})
+
+Template.header.onDestroyed(function() {})
+
+Template.header.helpers({
+  language() {
+    return TAPi18n.getLanguage().toLocaleUpperCase()
+  }
+})
+
+
